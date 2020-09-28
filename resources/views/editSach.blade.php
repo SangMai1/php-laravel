11475dd966f701fa0824ef1a480eed7f1d69785d
SangMai1 <vansang1532000@gmail.com>
2020-09-28 20:10:46 +0700

    thuesach

--- resources/views/editSach.blade.php
+++ resources/views/editSach.blade.php
@@ -1,6 +1,6 @@
 <form method="post" action=" {{ route('editSach2',[$sache->id])}}">
   @csrf
-  Id :<input type="text" name="id" value={{$sache->id}}><br>
+  Id :<input type="hide" name="id" value={{$sache->id}}><br>
   Ten sach :<input type="text" name="tensach" value={{$sache->tensach}}><br>
   So luong : <input type="text" name="soluong" value={{$sache->soluong}}><br>
 
--- resources/views/editThueSach.blade.php
+++ resources/views/editThueSach.blade.php
@@ -1,9 +1,9 @@
 <form method="post" action=" {{ route('editThueSach',[$thueSachs->id])}}">
   @csrf
-  Id :<input type="text" name="id" value={{$thueSachs->id}}><br>
+  Id :<input type="hide" name="id" value={{$thueSachs->id}}><br>
   Nguoi thue :<input type="text" name="nguoi_thue" value={{$thueSachs->nguoi_thue}}><br>
   So luong thue : <input type="text" name="soluong_thue" value={{$thueSachs->soluong_thue}}><br>
-  Ngay thue : <input type="text" name="ngay_thue" value={{$thueSachs->ngay_thue}}><br>
+  Ngay thue : <input type="date" name="ngay_thue" value={{$thueSachs->ngay_thue}}><br>
   Loai sach :
   <select name="idsach_thue">
     @foreach($sachId as $key => $value)
